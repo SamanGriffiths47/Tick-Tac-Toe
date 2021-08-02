6542a105c3cf6e226588af6ed8a1c76e9982201f
Sam'an <samangriffiths47@gmail.com>
2021-08-02 00:22:30 -0500

    v4

--- js/app.js
+++ js/app.js
@@ -1,55 +1,21 @@
-console.log('Hello frontend')
 // Global Variables Here
-let score = 0
+const gameStart = document.querySelector(`h3`)
+const start = document.body.getElementsByTagName('start')[0]
 let tickWins = document.querySelector(`.tick-wins`).innerHTML
 let toeWins = document.querySelector(`.toe-wins`).innerHTML
-let gameActive = false
-let currentPlayer = `x`
-let gameState = [``, ``, ``, ``, ``, ``, ``, ``, ``]
-const xBoard = [
-  document.querySelector(`.tick1`),
-  document.querySelector(`.tick2`),
-  document.querySelector(`.tick3`),
-  document.querySelector(`.tick4`),
-  document.querySelector(`.tick5`),
-  document.querySelector(`.tick6`),
-  document.querySelector(`.tick7`),
-  document.querySelector(`.tick8`),
-  document.querySelector(`.tick9`)
-]
-let xImage = [
-  document.querySelector(`#tick1`).style.opacity,
-  document.querySelector(`#tick2`).style.opacity,
-  document.querySelector(`#tick3`).style.opacity,
-  document.querySelector(`#tick4`).style,
-  document.querySelector(`#tick5`).style.opacity,
-  document.querySelector(`#tick6`).style.opacity,
-  document.querySelector(`#tick7`).style.opacity,
-  document.querySelector(`#tick8`).style.opacity,
-  document.querySelector(`#tick9`).style.opacity
-]
-const oBoard = [
-  document.querySelector(`.toe1`),
-  document.querySelector(`.toe2`),
-  document.querySelector(`.toe3`),
-  document.querySelector(`.toe4`),
-  document.querySelector(`.toe5`),
-  document.querySelector(`.toe6`),
-  document.querySelector(`.toe7`),
-  document.querySelector(`.toe8`),
-  document.querySelector(`.toe9`)
-]
-const oImage = [
-  document.querySelector(`#toe1`),
-  document.querySelector(`#toe2`),
-  document.querySelector(`#toe3`),
-  document.querySelector(`#toe4`),
-  document.querySelector(`#toe5`),
-  document.querySelector(`#toe6`),
-  document.querySelector(`#toe7`),
-  document.querySelector(`#toe8`),
-  document.querySelector(`#toe9`)
-]
+let gameActive = null
+let x = []
+let o = []
+let draws = []
+let currentPlayer = x
+let playCount = []
+let oPositions = []
+let xPositions = []
+let cells = document.getElementsByClassName(`cells`)
+let positionMatch = 0
+let newSec = document.createElement(`section`)
+let script = document.querySelector(`script`)
+
 const winCombos = [
   [0, 1, 2], // winning rows
   [3, 4, 5],
@@ -60,11 +26,94 @@ const winCombos = [
   [0, 4, 8], //winning diagonals
   [2, 4, 6]
 ]
-console.log(xImage[3])
+
 ////////////////////////////////
+
 // Functions For Game Logic Here
 
-////////////////////////////////
-// Event Listeners Here
+const startGame = function () {
+  document.body.removeChild(start)
+  gameActive = true
+}
 
+// Board Reset
+let reset = function () {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].innerHTML = ''
+  }
+}
+
+// End Check Functions
+let drawCheck = function () {
+  if (playCount.length === 9) {
+    draws.push(+1)
+    gameActive = false
+    reset()
+  }
+}
+let winCheck = function (positionArray) {
+  for (let i1 = 0; i1 < 8; i1++) {
+    positionMatch = 0
+    for (let i2 = 0; i2 < 3; i2++) {
+      if (positionArray.indexOf(winCombos.slice()[i1][i2]) > -1) {
+        positionMatch = positionMatch + 1
+      }
+      if (positionMatch === 3) {
+        currentPlayer.push(+1)
+        gameActive = false
+        reset()
+      } else drawCheck()
+    }
+  }
+}
+
+// Board Click Functions
+
+let replayClick = function () {
+  document.body.addEventListener(`click`, function () {
+    return (gameActive = true)
+  })
+}
+let addEndScreen = function () {
+  newSec.innerHTML = `<div class="winner">
+  <h3 class="win">Winner!</h3>
+  <p class="replay">replay</p>
+  </div>`
+  newSec.className = `win-message`
+  document.body.insertBefore(newSec, script)
+
+  replayClick()
+  document.body.removeChild(`.win-message`)
+  gameActive = true
+}
+
+// Event Listeners Here
+gameStart.addEventListener('click', startGame())
+
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', function (event) {
+    if (currentPlayer === x) {
+      event.target.innerHTML = `<img class='tick-sb' src='./images/tick.png' alt='tick'></div>`
+      xPositions.push(i)
+      playCount.push(``)
+      winCheck(xPositions)
+      currentPlayer = o
+    } else if (currentPlayer === o) {
+      event.target.innerHTML = `<img class='toe-sb' src='./images/big-toe.png' alt='toe'>`
+      oPositions.push(i)
+      playCount.push(``)
+      winCheck(oPositions)
+      currentPlayer = x
+    }
+  })
+}
+
+if (gameActive === false) {
+  addEndScreen()
+}
+if (gameActive !== null) {
+  console.log(`wow`)
+}
+
+////////////////////////////////
 ////////////////////////////////
